3b3f28689fd839d7bf12ccf10b1821efc81d184b
tejvuligonda <tejvuligonda@gmail.com>
2017-03-16 22:30:05 -0700

    Added scripts to purge the images and clusters db

---
+++ machine_learning/purge_images_db.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+import sys
+client = MongoClient('mongodb://hpmaharaja:Jaganath1@ds117869.mlab.com:17869/resang_users')
+db = client.resang_users
+try:
+    if (sys.argv[1] == 'dbadmin'):
+        db.images.remove({})
+    print('Purging complete.')
+except IndexError:
+    print('db is still intact. nothing removed')
+
